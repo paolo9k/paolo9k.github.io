0022c687d439efab1245eaa3a5fcac524aebb0a1
Paolo Grillier <paolo.grillier@sciacademy.org>
2023-09-13 15:19:33 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -61,8 +61,10 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("diamond",833,178,0,00)
+    createCollectable("max", 342,200,0,0)
+    createCollectable("kennedi",1000,460,0,0)
+    createCollectable("grace",500,569,0,0)
 
 
     // TODO 3
@@ -70,8 +72,8 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
+    createCannon("top",650,700,12,10)
+    createCannon(left,300,700,12,10)
 
 
     /////////////////////////////////////////////////

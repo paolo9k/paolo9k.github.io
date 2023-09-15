b064f91fb6d2561c7e4de49ee5abdc31411d7af8
Paolo Grillier <paolo.grillier@sciacademy.org>
2023-09-15 15:16:00 +0000

    <message about commit>

--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -2,7 +2,7 @@
 const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
 const friction = 1.5; // how much the player is slowed each frame
-const maxSpeed = 35; // maximum horizontal speed, not vertical
+const maxSpeed = 25; // maximum horizontal speed, not vertical
 const playerJumpStrength = 15; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 
